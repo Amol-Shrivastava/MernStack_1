3fc775d2644757ec49dd7c58f4fe70059b5542b8
amol <amolshrivastava448@gmail.com>
2022-12-11 20:39:31 +0530

    Registration working successfully

--- server/routes/AuthApi.js
+++ server/routes/AuthApi.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
 import UserModel from "../models/UserModel.js";
 
 const router = Router();
@@ -12,10 +13,22 @@ router.post("/register", async (req, res) => {
     res.status(406).json({ message: "User already exists" });
     return;
   } else {
-    const user = await UserModel({ email, password, firstName, lastName });
-    const savedUser = await user.save();
-    console.log(savedUser);
-    res.status(200).json({ message: "New User created successfully" });
+    //3a Generating Hash password
+    const saltRounds = 10;
+    const salt = await bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = await bcrypt.hashSync(password, salt);
+
+    //3b Storing hash password in db
+    const user = await UserModel({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+
+    //4. Save user into the db
+    await user.save();
+    res.status(201).json({ message: "New User created successfully" });
   }
 });
 
